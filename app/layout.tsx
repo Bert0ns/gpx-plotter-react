63763486dfdfd4af591cf40ae1ef1af5a8beb3e6
Bert0ns <121551954+Bert0ns@users.noreply.github.com>
2025-02-09 19:58:48 +0100

    feat: added vercel speed analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/app/components/atoms/Footer";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         {children}
       <Footer/>
       <Analytics/>
+      <SpeedInsights/>
       </body>
     </html>
   );
